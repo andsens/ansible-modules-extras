f356a06b8d583eccc42278cc4fe7fa6219151972
Anders Ingemann <aim@secoya.dk>
2017-07-07 21:32:36 +0200

    sensu_subscription: Fix return type for 2 cases
    state=absent would cause errors in 2 cases resulting in the error:
    "TypeError: 'bool' object is not iterable"

--- monitoring/sensu_subscription.py
+++ monitoring/sensu_subscription.py
@@ -111,7 +111,7 @@ def sensu_subscription(module, path, name, state='present', backup=False):
     if 'subscriptions' not in config['client']:
         if state == 'absent':
             reasons.append('`client.subscriptions\' did not exist and state is `absent\'')
-            return changed
+            return changed, reasons
         config['client']['subscriptions'] = []
         changed = True
         reasons.append('`client.subscriptions\' did not exist')
@@ -119,7 +119,7 @@ def sensu_subscription(module, path, name, state='present', backup=False):
     if name not in config['client']['subscriptions']:
         if state == 'absent':
             reasons.append('channel subscription was absent')
-            return changed
+            return changed, reasons
         config['client']['subscriptions'].append(name)
         changed = True
         reasons.append('channel subscription was absent and state is `present\'')
